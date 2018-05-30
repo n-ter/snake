6ba85982036281c421e56c5c4672f73fa582f921
N-ter <terebova@jl.elama.ru>
2018-05-30 22:03:56 +0300

    движение змейки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -29,6 +29,7 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+            snake.Move();
         }
     }
 }
